34ed5d2e1e98e404761a145a8da470dcab4ae9f5
Cara Peckens <cpeckens@gmail.com>
2013-03-08 11:48:00 -0500

    Revert "Resty Fail"
    This reverts commit 19d35e7cf40e6e75ca8b9ae28593695c3cdb44bc.

--- parts-microsoft-academic.php
+++ parts-microsoft-academic.php
@@ -1,23 +1,15 @@
 <?php $author_id = get_post_meta($post->ID, 'ecpt_microsoft_id', true);
 		$api_key = '590653d5-7996-40b8-ad51-34426603139c';
-	require (TEMPLATEPATH . '/assets/functions/Resty.php'); 
-		$http = new Resty();
-		$http->debug(true);
-        $http->setBaseUrl("http://academic.research.microsoft.com/json.svc");
-          $queryData = array(
-            "AppID" => $api_key,
-            "AuthorId" => $author_id,
-            "ResultObjects" => "publication", 
-            "PublicationContent" => "title",
-            "ResultObjects" => "publication", 
-            "OrderType" => "year", 
-            "StartIdx" => "1",
-            "EndInx" => "25", 
-            );
-        $response = $http->get("/search", $queryData);
-        $response->parse_body(true);
-
-		$microsoft_pubs = $response['d']['Publication'];
+		$microsoft_url = 'http://academic.research.microsoft.com/json.svc/search?AppId=' . $api_key . '&AuthorID=' . $author_id . '&ResultObjects=publication&PublicationContent=title,ConferenceAndJournalInfo&OrderType=year&StartIdx=1&EndIdx=25';
+			$rCURL = curl_init();
+				curl_setopt($rCURL, CURLOPT_URL, $microsoft_url);
+				curl_setopt($rCURL, CURLOPT_HEADER, 0);
+				curl_setopt($rCURL, CURLOPT_RETURNTRANSFER, 1);
+		
+		$microsoft_call = curl_exec($rCURL);
+		curl_close($rCURL);
+		$microsoft_results = json_decode( $microsoft_call, true );
+		$microsoft_pubs = $microsoft_results['d']['Publication'];
 		foreach($microsoft_pubs['Result'] as $microsoft_pub) { 
 
 ?>	
