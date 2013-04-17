25aa8c8e4150c56960d9de65e10c824a16440c45
Cara Peckens <cpeckens@gmail.com>
2013-04-17 15:16:23 -0400

    javascript condensed and minified - need to test

--- parts-script-initiators.php
+++ parts-script-initiators.php
@@ -1,34 +1,11 @@
-<!***********FOUNDATION SCRIPTS**************>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.mediaQueryToggle.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.forms.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.reveal.js"></script> <!-- ALL -->
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.navigation.js"></script> <!-- ALL -->
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.buttons.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.tabs.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.tooltips.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.accordion.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.placeholder.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.alerts.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.topbar.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.joyride.js"></script>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.clearing.js"></script> <!-- Photo Index -->
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.magellan.js"></script>
-  
-<!***********ALL PAGES**************>
-  <script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/app.js"></script>
-  <!-- Add "external" class to hyperlinks not on the krieger.jhu.edu domain -->
-  <script>
-	  jQuery('a').filter(function() {
-		  return this.hostname && this.hostname !== location.hostname;
-	  }).addClass("external_link");
-  </script>
+<!***********ALL PAGES**************>  
+<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/min.foundation.js"></script> 
+<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/app.js"></script>
     
 <!***********DIRECTORY**************>
 <?php $theme_option = flagship_sub_get_global_options();
 if ( is_page_template( 'template-people-directory.php' ) && $theme_option['flagship_sub_directory_search']  == '1' )  { ?>
-  	<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.isotope.js"></script>
-  	<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.quicksearch.js"></script>  	
-  	<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/page.directory.js"></script>
+  	<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/min.page.directory.js"></script>
   	<script>
 	    var $j = jQuery.noConflict();
 	    $j(window).load(function() {
@@ -69,7 +46,7 @@ if ( is_page_template( 'template-people-directory.php' ) && $theme_option['flags
 
 <!***********HOMEPAGE**************>
 <?php if ( is_front_page()) { ?>
-	<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/jquery.foundation.orbit.js"></script>
+	<script src="<?php echo get_template_directory_uri() ?>/assets/javascripts/min.foundation.orbit.js"></script>
 	<script>
 		var $l = jQuery.noConflict();
 		$l(window).load(function() {
