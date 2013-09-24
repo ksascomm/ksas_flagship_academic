693e25741c29f4e3550bf15c3c7a55889a201b17
Cara Peckens <cpeckens@gmail.com>
2013-09-24 10:02:24 -0400

    Version 2.0
    IE7/IE8 Compatibility mode fix

---
+++ README.md
@@ -0,0 +1,16 @@
+theme_flagship_academic
+======================
+**Current Version:** 2.0
+
+###Theme Options
+*   Homepage news feed title
+*   Quantity of news items on homepage
+*   Homepage slider choice (vertical or horizontal caption)
+*   Breadcrumb navigation choice
+*   People directory search option
+*   Google analytics setting
+*   Google search appliance setting
+*   Site Executive calendar address setting
+*   Quicklinks from another site setting
+*   Site ID for Quicklinks
+*   Custom footer/copyright text
--- header.php
+++ header.php
@@ -1,11 +1,5 @@
 <!DOCTYPE html>
-
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-
+<html lang="en">
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
@@ -31,6 +25,7 @@
   <!-- IE Fix for HTML5 Tags -->
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
   <![endif]-->
   <?php include_once("parts-analytics.php"); ?>
 </head>
