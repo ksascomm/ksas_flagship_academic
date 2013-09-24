2653c5a4e1a66bb0ccbdb762560ec8ee53ae93fc
Cara Peckens <cpeckens@gmail.com>
2013-09-24 09:53:33 -0400

    Faculty books
    Add second author display to archive and single views

--- single-category-books.php
+++ single-category-books.php
@@ -3,7 +3,7 @@
 						<?php the_post_thumbnail('medium', array('class'	=> "floatleft")); ?>
 					<?php } ?>
 					<?php $faculty_post_id = get_post_meta($post->ID, 'ecpt_pub_author', true);
-							$faculty_post_id2 = get_post_meta($post->ID, 'ecpt_pub_author2', true); ?>
+						  $faculty_post_id2 = get_post_meta($post->ID, 'ecpt_pub_author2', true); ?>
 				<?php if ( get_post_meta($post->ID, 'ecpt_pub_link', true) ) :?>
 					<a href="http://<?php echo get_post_meta($post->ID, 'ecpt_pub_link', true); ?>">
 				<?php endif; ?>
@@ -14,12 +14,10 @@
 					<?php if ( get_post_meta($post->ID, 'ecpt_publisher', true) ) :?>, <?php echo get_post_meta($post->ID, 'ecpt_publisher', true);  endif; ?>
 				</h6>
 				<p><b><a href="<?php echo get_permalink($faculty_post_id); ?>"><?php echo get_the_title($faculty_post_id); ?> 
-				<?php if ( get_post_meta($post->ID, 'ecpt_pub_role', true)) :?>, <?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); endif; ?>
-				</a>
-				<?php if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on') { ?><br>
-					<a href="<?php echo get_permalink($faculty_post_id2); ?>"><?php echo get_the_title($faculty_post_id2); ?> 
-						<?php if ( get_post_meta($post->ID, 'ecpt_pub_role2', true)) :?>, <?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); endif;  ?>
-					</a>
+				<?php if ( get_post_meta($post->ID, 'ecpt_pub_role', true)) :?>, <?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); endif; ?></a></b>
+				<?php if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on') { ?><br></b>
+					<a href="<?php echo get_permalink($faculty_post_id2); ?>"><?php echo get_the_title($faculty_post_id2); ?>,&nbsp;<?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); ?>
+					</a></a>
 				<?php } ?>
-				</b></p>
+				</p>
 				<?php the_content(); ?>		
